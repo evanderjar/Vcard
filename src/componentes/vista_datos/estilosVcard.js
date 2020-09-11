dc4c0fb88e8ba2200012cb329ac2c80da71acd2a
Zeto96 <47188143+Zeto96@users.noreply.github.com>
2020-09-11 09:54:47 -0400

    vcard

--- src/componentes/vista_datos/estilosVcard.js
+++ src/componentes/vista_datos/estilosVcard.js
@@ -1,12 +1,66 @@
+import fondos_buscar_vcard from '../../assets/Imagen-background1.jpg';
+
 export const VcardStile = {
-    backgrond:{
-        position: "absolute",
-        zIndex: "2",
-        width: "100%",
-        height: "100%",
-    },
     contenedor:{
         position: "absolute",
         zIndex: "2",
+        paddingLeft: "42%"
+    },
+    contenedor2:{
+        paddingBottom: "285px"
+    },
+    todo:{
+        width: "100%",
+        height: "100%"
+    },
+    back:{
+        width: "100%",
+        height: "100%",
+        position: "fixed",
+        zIndex: "1",
+        backgroundSize: "cover",
+        backgroundImage: `url(${fondos_buscar_vcard})`
+    },
+    imag:{
+        width: "200px",
+        height: "200px",
+        borderRadius: "50%",
+        marginBottom: "15px"
+    },
+    tarjeta:{
+        marginLeft: "374px",
+        paddingRight: "64px",
+    },
+    nombre:{
+        justifyContent: "center"
+    },
+    nombre2:{
+        paddingRight: "10px",
+        color: "white"
+    },
+    nombre3:{
+        paddingRight: "10px",
+        color: "white"
+    },
+    tlf:{
+        color: "white"
+    },
+    url:{
+        marginLeft: "150px"
+    },
+    redes:{
+        textAlign: "center"
+    },
+    Tw:{
+        paddingRight: "13px"
+    },
+    Fb:{
+        paddingRight: "13px"
+    },
+    Ins:{
+        paddingRight: "13px"
+    },
+    LK:{
+        paddingRight: "13px"
     }
 } 
--- src/componentes/vista_datos/vista_datos.js
+++ src/componentes/vista_datos/vista_datos.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import {
   CContainer,
+  CRow
 } from '@coreui/react'
 import { useParams } from 'react-router-dom'
 
@@ -10,6 +11,7 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 // import 'mdbreact/dist/css/mdb.css';
 import{ VcardStile } from './estilosVcard';
 import backVcard from '../../assets/Imagen-background1.jpg';
+import Navbar from '../navbar/navbar'
 
 
 function VistaDatos() {
@@ -68,71 +70,86 @@ function VistaDatos() {
   if(mostrarReturn){
     return (
       <Fragment>
-        <img style={VcardStile.backgrond} src={backVcard} alt="backVcard"></img>
-        <CContainer style={VcardStile.contenedor}>
-          <div>
-        {usuario.foto_perfil === "" || usuario.twitter === undefined ? "":
-          <img src={usuario.foto_perfil} alt="foto perfil" Style="width: 70px;" required></img>
-        }
-        <h1>Usuario: {usuario.nombre}</h1>
-        <h2>Apellido: {usuario.apellido}</h2>
-        <h2>Telefono: {usuario.telefono}</h2>
-        {/* <div>
-          <h2 Style='display: contents;'>Su ruta es</h2>
-          <a href={`http://tienda.deproinf.com.ve/#/${usuario.nombre_ruta}`}>http://tienda.deproinf.com.ve/#/{usuario.nombre_ruta}</a>
-        </div> */}
+        <section style = {VcardStile.back}>
+         <div style={VcardStile.todo}>
+        <Navbar/>
+        {/* <div className="col-md-6" style={VcardStile.tarjeta}> */}
+            {/* <div className="card tex-center">
+              <div className="card-header" style={VcardStile.contenedor2}> */}
+              <CRow style={VcardStile.nombre}>
+              {usuario.foto_perfil === "" || usuario.twitter === undefined ? "":
+              <img src={usuario.foto_perfil} alt="foto perfil" style={VcardStile.imag} required></img>
+              }
+              </CRow>
+              <CRow style={VcardStile.nombre}>
+              <h1 class="text-center" style={VcardStile.nombre2}>Usuario: {usuario.nombre}</h1><h1 style={VcardStile.nombre3}>Apellido: {usuario.apellido}</h1>
+              </CRow>
+              <CRow style={VcardStile.nombre}>
+              <h2 style={VcardStile.tlf}>Telefono: {usuario.telefono}</h2>
+              </CRow>
+              {/* <label for="basic-url">Tu Url</label> */}
+              <p></p>
+             
+              <div  style={VcardStile.nombre} class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="basic-addon3">http://tienda.deproinf.com.ve/#/</span>
+                </div>
+                <input type="text" id="basic-url" aria-describedby="basic-addon3" value={usuario.nombre_ruta} readOnly/>
+              </div>
+  
+                {/* <div>
+                <h2 Style='display: contents;'>Su ruta es</h2>
+                <a href={`http://tienda.deproinf.com.ve/#/${usuario.nombre_ruta}`}>http://tienda.deproinf.com.ve/#/{usuario.nombre_ruta}</a>
+                </div> */}
+                <div style={VcardStile.redes}>
+                {usuario.twitter === "" || usuario.twitter === undefined ? '': 
+                  <a style={VcardStile.Tw} target="_blank" href={usuario.twitter} rel="noopener noreferrer">
+                    <i class="fab fa-twitter-square" Style="font-size: 35px !important; color:#00acee !important"></i>
+                  </a>
+                }
 
-        <label for="basic-url">Tu Url</label>
-          <div class="input-group mb-3">
-            <div class="input-group-prepend">
-              <span class="input-group-text" id="basic-addon3">http://tienda.deproinf.com.ve/#/</span>
-            </div>
-            <input type="text" id="basic-url" aria-describedby="basic-addon3" value={usuario.nombre_ruta} readOnly/>
-          </div>
-        
-        <div>
-          {usuario.twitter === "" || usuario.twitter === undefined ? '': 
-            <a target="_blank" href={usuario.twitter} rel="noopener noreferrer">
-              <i class="fab fa-twitter-square" Style="font-size: 35px !important; color:#00acee !important"></i>
-            </a>
-          }
+                {usuario.instagram === "" || usuario.instagram === undefined ? '': 
+                  
+                  <a style={VcardStile.Ins} target="_blank" href={usuario.instagram} rel="noopener noreferrer">
+                    <i class="fab fa-instagram" Style="font-size: 35px !important; color:#3f729b !important"></i>
+                  </a>
+                }
 
-          {usuario.instagram === "" || usuario.instagram === undefined ? '': 
-            
-            <a target="_blank" href={usuario.instagram} rel="noopener noreferrer">
-              <i class="fab fa-instagram" Style="font-size: 35px !important; color:#3f729b !important"></i>
-            </a>
-          }
-          
-          {usuario.facebook === "" || usuario.facebook === undefined ? '': 
-            <a target="_blank" href={usuario.facebook} rel="noopener noreferrer">
-              <i class="fab fa-facebook" Style="font-size: 35px !important; color:#3b5998 !important"></i>
-            </a>
-          }
+                {usuario.facebook === "" || usuario.facebook === undefined ? '': 
+                  <a style={VcardStile.Fb} target="_blank" href={usuario.facebook} rel="noopener noreferrer">
+                    <i class="fab fa-facebook" Style="font-size: 35px !important; color:#3b5998 !important"></i>
+                  </a>
+                }
 
-          {usuario.linkedin === "" || usuario.linkedin === undefined ? '': 
-            <a target="_blank" href={usuario.linkedin} rel="noopener noreferrer">
-              <i class="fab fa-linkedin" Style="font-size: 35px !important; color:#3b5998 !important"></i>
-            </a>
-          }
-          {usuario.web === "" || usuario.web === undefined ? '': 
-            <a target="_blank" href={usuario.web} rel="noopener noreferrer">
-              <i class="fas fa-globe-americas" Style="font-size: 35px !important;"></i>
-            </a>
-          }
-        </div>
-        </div>
-        </CContainer>
-        <a href={compartir_facebook} target="_blank">
-          Share on Facebook
-        </a>
-        <div></div>
-        <a link rel="canonical"
-          class="twitter-share-button"
-          href="https://twitter.com/intent/tweet?text=tienda.deproinf.com.ve/,/9966" target="_blank">
-          Share on twiter
-        </a>
-        
+                {usuario.linkedin === "" || usuario.linkedin === undefined ? '': 
+                  <a style={VcardStile.LK} target="_blank" href={usuario.linkedin} rel="noopener noreferrer">
+                    <i class="fab fa-linkedin" Style="font-size: 35px !important; color:#3b5998 !important"></i>
+                  </a>
+                }
+                {usuario.web === "" || usuario.web === undefined ? '': 
+                  <a target="_blank" href={usuario.web} rel="noopener noreferrer">
+                    <i class="fas fa-globe-americas" Style="font-size: 35px !important;"></i>
+                  </a>
+                }
+                </div>
+                <p></p>
+                <CRow style={VcardStile.nombre}>
+                <a style={VcardStile.nombre2} href={compartir_facebook} target="_blank">
+                  Share on Facebook
+                  </a>
+                  <div></div>
+                  <a style={VcardStile.nombre3} link rel="canonical"
+                  class="twitter-share-button"
+                  href="https://twitter.com/intent/tweet?text=tienda.deproinf.com.ve/,/9966" target="_blank">
+                  Share on twiter
+                  </a>
+                  </CRow>
+              </div>
+            {/* </div>
+          </div> */}
+
+        {/* </div> */}
+        </section>
       </Fragment>
     );
   }else{
